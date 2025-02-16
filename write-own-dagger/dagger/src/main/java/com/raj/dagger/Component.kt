d629ed6d03ac33851397c4d2d376c6964c199020
Raj Kumar Singh <rksinghj4@gmail.com>
2025-02-16 21:56:39 +0530

    Add Component annotation

---
+++ write-own-dagger/dagger/src/main/java/com/raj/dagger/Component.kt
@@ -0,0 +1,7 @@
+package com.raj.dagger
+
+@Target(AnnotationTarget.CLASS)//Class, interface or object, annotation class is also included
+@MustBeDocumented
+@Retention(AnnotationRetention.SOURCE)
+annotation class Component
+
--- write-own-dagger/settings.gradle.kts
+++ write-own-dagger/settings.gradle.kts
@@ -15,4 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "CustomMiniDaggerSample"
 include(":app")
- 
+include(":dagger")
