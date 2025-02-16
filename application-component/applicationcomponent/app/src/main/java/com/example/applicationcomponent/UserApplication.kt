85a6ed21960ab4b650ea4af83b78970dbc6a104a
Raj Kumar Singh <rksinghj4@gmail.com>
2025-02-16 18:07:53 +0530

    Add a description comment

--- application-component/applicationcomponent/app/src/main/java/com/example/applicationcomponent/UserApplication.kt
+++ application-component/applicationcomponent/app/src/main/java/com/example/applicationcomponent/UserApplication.kt
@@ -2,10 +2,17 @@ package com.example.applicationcomponent
 
 import android.app.Application
 
-class UserApplication: Application() {
+class UserApplication : Application() {
+    /**
+     * UserRegistrationComponent object is created here UserApplication class
+     * so it's scope/life is tide with application object
+     * So all providers inside the @module class using the
+     * same scope annotation(i.e. @Singleton) as @component interface
+     * will have same life (i.e. created only once in life time/marked scope)
+     */
     lateinit var userRegistrationComponent: UserRegistrationComponent
     override fun onCreate() {
         super.onCreate()
-        userRegistrationComponent = DaggerUserRegistrationComponent.factory().create( 4, 111)
+        userRegistrationComponent = DaggerUserRegistrationComponent.factory().create(4, 111)
     }
 }
--- application-component/applicationcomponent/app/src/main/java/com/example/applicationcomponent/UserRegistrationComponent.kt
+++ application-component/applicationcomponent/app/src/main/java/com/example/applicationcomponent/UserRegistrationComponent.kt
@@ -1,4 +1,5 @@
 package com.example.applicationcomponent
+
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -7,17 +8,27 @@ import javax.inject.Singleton
  * All @Singleton annotated  providers and binder will be single within the Component scope.
  */
 @Singleton
-@Component (modules = [UserRepositoryModule::class, NotificationServiceModule::class, AnalyticsModule::class])
-interface UserRegistrationComponent {
+@Component(modules = [UserRepositoryModule::class, NotificationServiceModule::class, AnalyticsModule::class])
+interface UserRegistrationComponent {//Component is a bride between provider and consumer
 
     @Component.Factory
-    interface Factory  {
+    interface Factory {
         /**
          * 11- Now component have two integer values with qualifiers and
          * it can supply these anywhere needed with required qualifier.
          */
-        fun create(@RetryQualifier @BindsInstance retryCount1: Int,  @EmailRetryQualifier @BindsInstance emailRetryCount: Int): UserRegistrationComponent
+        fun create(
+            @RetryQualifier @BindsInstance retryCount1: Int,
+            @EmailRetryQualifier @BindsInstance emailRetryCount: Int
+        ): UserRegistrationComponent
     }
 
-    fun inject(activity: MainActivity) //Pass the consumer.
+    /**
+     * If providers and consumers are part of same bucket/bridge/component then we don't need to write
+     * Signature methods here in Component. Just write a method with Consumer parameter/Activity/Fragment
+     */
+
+    fun inject(activity: MainActivity) //Pass the consumer
+
+
 }
